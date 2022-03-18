9c1fdd034898d75a1882b6a8752231060eda5f3a
Mishell Yagual <54877658+mdyagual@users.noreply.github.com>
2022-03-17 22:09:19 -0500

    Cambio del tipo de dato del id a String para facilidad del ejercicio

--- src/main/java/com/example/demo/CsvUtilFile.java
+++ src/main/java/com/example/demo/CsvUtilFile.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import com.example.demo.models.Player;
 import com.opencsv.CSVReader;
 import com.opencsv.exceptions.CsvException;
 
@@ -18,7 +19,7 @@ public class CsvUtilFile {
         try (CSVReader reader = new CSVReader(new FileReader(uri.getFile()))) {
             List<String[]> registers = reader.readAll();
             registers.forEach(strings -> list.add(new Player(
-                    Integer.parseInt(strings[0].trim()),
+                    strings[0].trim(),
                     strings[1],
                     Integer.parseInt(Optional.of(strings[2].trim()).filter(h -> !h.isBlank()).orElse("0")),
                     strings[3],
--- src/test/java/com/example/demo/CSVUtilTest.java
+++ src/test/java/com/example/demo/CSVUtilTest.java
@@ -1,19 +1,12 @@
 package com.example.demo;
 
-import com.opencsv.CSVReader;
-import com.opencsv.exceptions.CsvException;
+import com.example.demo.models.Player;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.*;
-import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class CSVUtilTest {
 
